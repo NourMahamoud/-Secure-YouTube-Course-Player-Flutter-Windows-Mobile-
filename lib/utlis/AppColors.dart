3d13f0af280fc3cfb5317a34080b2a7a04dd659e
NourMahamoud <nourmahamoud737@gmail.com>
2026-02-21 23:37:50 +0200

    Remove ApiService from remote only

---
+++ lib/utlis/AppColors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors{
+ static Color logofirstColor = Color(0xff042A2F) ;
+ static Color logosecondColor = Color(0xff083B42) ;
+
+}
---
+++ lib/utlis/ImagesPath.dart
@@ -0,0 +1,13 @@
+
+class AppImages{
+  static  String SplachImagePath = 'assets/images/SplachScreenPhoto.svg.png' ;
+  static  String LogoPath = 'assets/images/logo.png' ;
+  static String SpPath = 'assets/images/SP.png' ;
+  static String spgPath= 'assets/images/SPG.png' ;
+  static String iconPath= 'assets/images/Icon.Svg' ;
+  static String imgPath= 'assets/images/img.png' ;
+  static String noCoursesPath= 'assets/images/Couresesnotfoundpic.png' ;
+
+
+
+}
